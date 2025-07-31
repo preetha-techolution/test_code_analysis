9bc6336ff6306ed36f1e31fb63b629d112e82576
Preetha <preetha.a@techolution.com>
2025-07-31 17:42:59 +0530

    deleted rooms

--- rooms.py
+++
@@ -1,9 +0,0 @@
-def check_availability(cursor):
-    cursor.execute("SELECT room_number FROM rooms WHERE is_occupied = 0")
-    return [row[0] for row in cursor.fetchall()]
-
-def book_room(cursor, room_number):
-    cursor.execute("UPDATE rooms SET is_occupied = 1 WHERE room_number = ?", (room_number,))
-
-def checkout_room(cursor, room_number):
-    cursor.execute("UPDATE rooms SET is_occupied = 0 WHERE room_number = ?", (room_number,))
